8af501c6cfc411eaadf88887b495080dd03893eb
Nate <nategt47@gmail.com>
2023-03-06 00:57:39 -0500

    Updates

---
+++ precache-manifest.ccc1b76a2947b6bb6335cd81b350c918.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f8c8c20a5e502fdda5e",
+    "url": "/random-quote-generator/static/js/main.4f8c8c20.chunk.js"
+  },
+  {
+    "revision": "31c910c49f3502a03647",
+    "url": "/random-quote-generator/static/js/1.31c910c4.chunk.js"
+  },
+  {
+    "revision": "7de484aaff74c1507f8a",
+    "url": "/random-quote-generator/static/js/runtime~main.7de484aa.js"
+  },
+  {
+    "revision": "669e3e9b2eca6179c95b9a9813f5890d",
+    "url": "/random-quote-generator/index.html"
+  }
+];
